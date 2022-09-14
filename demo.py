b653aa501cfb850370a231013017d61afd97c2eb
icd_coding_helper <43208033+icd-coding@users.noreply.github.com>
2022-09-14 11:11:11 +0200

    ClinCode
    First commit of DEMO for user study

---
+++ demo.py
@@ -0,0 +1,111 @@
+'''
+@author: taridzo, 2022
+First version of a DEMO for clincode. It uses a fine-tuned model and flask for the view.
+'''
+import sys
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from transformers import BertTokenizer
+from keras.models import load_model
+from flask import Flask
+from flask import request
+from flask import render_template
+from bs4 import BeautifulSoup
+
+from demo_fuzzy_sentence import get_fuzzy_sentence_top_n
+
+MODEL_NAME = "static/swedClinBert"
+icd_desc_f = 'static/icd/gastro_se_blk.csv'
+choices = pd.read_csv(icd_desc_f, sep=',', header=0)
+modelx = load_model('static/model')
+
+label_dict = ['K209', 'K210', 'K219', 'K222', 'K250', 'K251', 'K253', 'K259', 'K260', 'K261', 'K263', 'K279', 'K295',
+              'K297', 'K298', 'K299', 'K309', 'K310', 'K317', 'K351', 'K352', 'K353', 'K358', 'K359', 'K369', 'K379',
+              'K403', 'K409', 'K420', 'K429', 'K430', 'K432', 'K435', 'K439', 'K449', 'K469', 'K500', 'K501', 'K508',
+              'K509', 'K510', 'K512', 'K519', 'K523', 'K528', 'K529', 'K550', 'K560', 'K561', 'K562', 'K564', 'K565',
+              'K566', 'K567', 'K571', 'K572', 'K573', 'K578', 'K579', 'K580', 'K590', 'K591', 'K602', 'K603', 'K604',
+              'K610', 'K611', 'K613', 'K621', 'K622', 'K624', 'K625', 'K626', 'K630', 'K631', 'K632', 'K635', 'K649',
+              'K650', 'K658', 'K660', 'K703', 'K746', 'K750', 'K754', 'K760', 'K766', 'K768', 'K769', 'K800', 'K801',
+              'K802', 'K803', 'K804', 'K805', 'K810', 'K819', 'K830', 'K831', 'K850', 'K851', 'K852', 'K858', 'K859',
+              'K860', 'K861', 'K862', 'K863', 'K868', 'K900', 'K912', 'K913', 'K914', 'K920', 'K921', 'K922']
+
+MAX_LENGTH = 512
+tokenizer = BertTokenizer.from_pretrained(MODEL_NAME,
+                                          add_special_tokens=True,
+                                          max_length=MAX_LENGTH,
+                                          pad_to_max_length=True)
+
+
+def tokenize(sentences, tokenizer):
+    input_ids, input_masks, input_segments = [], [], []
+    for sentence in tqdm(sentences):
+        inputs = tokenizer.encode_plus(sentence,
+                                       add_special_tokens=True,
+                                       max_length=MAX_LENGTH,
+                                       pad_to_max_length=True,
+                                       return_attention_mask=True,
+                                       return_token_type_ids=True,
+                                       truncation=True)
+        input_ids.append(inputs['input_ids'])
+        input_masks.append(inputs['attention_mask'])
+        input_segments.append(inputs['token_type_ids'])
+    return np.asarray(input_ids, dtype='int32'), np.asarray(input_masks, dtype='int32')
+
+
+def get_top_n(clinical_note):
+    if not isinstance(clinical_note, str): return []
+    if not clinical_note: return []
+    fuzzy_top_n = get_fuzzy_sentence_top_n(clinical_note, choices)
+    clinical_note = tokenize([clinical_note], tokenizer)
+    y_test_probs = modelx.predict(clinical_note)
+    bert_top_n = [x for _, x in sorted(zip(y_test_probs[0], label_dict), key=lambda pair: pair[0], reverse=True)][:5]
+
+    return list(set(bert_top_n + fuzzy_top_n[:2]))
+
+
+def get_finnkode():
+    global code
+    ICD_DESCRIPTIONS = "static/icd/converted.txt"
+    f = open(ICD_DESCRIPTIONS, "r")
+    soup = BeautifulSoup(f.read(), features="html.parser")
+    record = []
+    rec = soup.find_all("span", {"class": "kodekode"})
+    for recs in rec:
+        code = recs.find("span", {"class": "kodenr"})
+        title = recs.find("span", {"class": "hovedterm"})
+        subtitles = recs.find_all("span", {"class": "tekst"})
+        count = 0
+        subs = []
+        for sub_t in subtitles:
+            subs.append(['teksLeonardo Da Vinci kunstwerket' + str(count), sub_t.text])
+            count += 1
+        record.append([['code', code.text + ' | ' + title.text], subs])
+    return record
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template('icd_demo.html')
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'POST':
+        clinical_note = request.form['notes']
+        top_n = get_top_n(clinical_note)
+        top_n_results = []
+        for icd_code in top_n:
+            icd_desc = choices.loc[choices.code == icd_code, 'description'].values[0]
+            top_n_results.append([icd_code, icd_code + ' | ' + icd_desc])
+
+        return render_template('icd.html',
+                               top_results=top_n_results,
+                               clinical_note=clinical_note,
+                               finnkode=get_finnkode())
+
+
+app.run(debug=True, host='0.0.0.0', use_reloader=False)
---
+++ demo_fuzzy_sentence.py
@@ -0,0 +1,64 @@
+'''
+@author: taridzo, 2021
+Fuzzy string matching based on sentence similarity.
+'''
+import sys
+from thefuzz import fuzz
+import pandas as pd
+from nltk.tokenize import sent_tokenize
+# nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+import string
+from tqdm import tqdm
+
+tqdm.pandas()
+import spacy
+
+spacy.prefer_gpu()
+nlp = spacy.load('sv_core_news_lg')
+# nltk.download('stopwords')
+stop_words = set(stopwords.words('swedish'))
+stemmer = SnowballStemmer("swedish")
+punct = string.punctuation
+
+SIMILARITY_THRESHOLD = 10
+
+
+def remove_punctuation(text):
+    return text.translate(str.maketrans('', '', punct))
+
+
+def remove_stopwords(text):
+    return " ".join([word for word in str(text).split() if word not in stop_words])
+
+
+def get_similarity(sentence, icd_description):
+    return fuzz.partial_ratio(str(sentence).lower(), str(icd_description).lower())
+
+
+def get_top_n(letter, choices):
+    assigned_codes = pd.DataFrame(columns=['description', 'code', 'block', 'similarity'])
+    sentences = sent_tokenize(letter)
+    for sentence in sentences:
+        sentence = remove_stopwords(sentence)
+        sentence = remove_punctuation(sentence)
+        choices['similarity'] = choices.description.apply(lambda x: get_similarity(sentence, x))
+        choices = choices.loc[choices['similarity'] >= SIMILARITY_THRESHOLD]
+        choices = choices.sort_values(['code', 'similarity'], ascending=False).groupby('code').head(1)
+        assigned_codes = pd.concat([assigned_codes, choices], ignore_index=True)
+
+    assigned_codes = assigned_codes.sort_values(['code', 'similarity'], ascending=False).groupby('code').head(1)
+
+    if assigned_codes.empty:
+        print("EMPTY: returning popular codes...")
+        return ['K567', 'K573', 'K358', 'K590', 'K800']
+    else:
+        assigned_codes['similarity'] = assigned_codes['similarity'].astype(int)
+        resx = assigned_codes.nlargest(5, 'similarity')
+        return resx.code.to_list()
+
+
+def get_fuzzy_sentence_top_n(txt, df):
+    df.description = df.description.str.lower()
+    return get_top_n(str.lower(txt), df)
